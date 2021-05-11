e25d9efa50101264182e23069a364cdf84e9067d
quoctoanpk2511 <quoctoanpk2511@gmail.com>
2021-05-12 00:07:00 +0700

    edit Similarity Scorer

--- base/scores/similarities.py
+++ base/scores/similarities.py
@@ -1,10 +1,9 @@
 import abc
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
 
 class SimilarityScorer:
     """
-    Abstract class for score similarity.
+    Abstract class for compute the similarity of the entities.
     """
 
     def matcher(self, matcher):
@@ -13,45 +12,50 @@ class SimilarityScorer:
     @abc.abstractmethod
     def distance_score(self):
         """
+        Compute similarity.
 
-        Returns: Dict
-
+        Returns:
+        -------
+        similarity_matrix : dict
+            An arary with shape of input array
         """
 
     def score(self):
         """
+        Get each data by for loop and call distance_score(),
+        save results to matcher after calling merge_matrix_with_weight_of_features().
 
         Returns: None
-
         """
-        similarity_dict = self.distance_score()
-        self.merge_matrix_with_weight_of_features(similarity_dict)
+        similarity_dict = {}
+        for feature, vector_matrix in self.matcher.vectorizer.vectorize().items():
+            similarity_dict[feature] = self.distance_score(vector_matrix)
+        self.matcher.similarity_matrix = self.merge_matrix_with_weight_of_features(similarity_dict)
 
     def merge_matrix_with_weight_of_features(self, similarity_dict):
         """
+        Merge matrix into one with its weight.
 
         Args:
-            similarity_dict: Dict
+            similarity_dict: dict
 
-        Returns: None
-
+        Returns:
+        -------
+        similarity_matrix: array
+            Similarity matrix of entities.
         """
         shape = self.matcher.get_count_entity()
         similarity_matrix = np.zeros((shape, shape))
         for feature, distance in similarity_dict.items():
             similarity_matrix += similarity_dict[feature] * self.matcher.join_features[feature]
-        self.matcher.similarity_matrix = similarity_matrix
+        return similarity_matrix
 
+
+from sklearn.metrics.pairwise import cosine_similarity
 class Cosine_Similarity(SimilarityScorer):
 
-    def distance_score(self):
+    def distance_score(self, vector_matrix):
         """
-
-        Returns: Dict
-
+        Compute cosine similarity.
         """
-        similarity_dict = {}
-        for feature, term_matrix in self.matcher.vectorizer.vectorize().items():
-            distance = cosine_similarity(term_matrix)
-            similarity_dict[feature] = distance
-        return similarity_dict
+        return cosine_similarity(vector_matrix)
